a79eee538555ce2101b61908749c4ecb41a0ca7c
gregor-d <didenko_g@gmx.de>
2026-06-09 22:54:03 +0200

    refactor: update configuration for tile JSON and raster source handling

--- frontend/static/config.js
+++ frontend/static/config.js
@@ -1,9 +1,7 @@
 const CONFIG = {
-    // apiBaseUrl: "http://127.0.0.1:8000",
-    apiBaseUrl: "http://167.233.20.236:8000",
-    apiBaseUrl: "https://tiles.alleinseinkarte.de",
-    // rasterCog: "sachsen_raster_cog.tif",
-    rasterCog: "germany_raster_web_optimized.tif",
-    rasterStack: "sachsen_raster_stack_web.tif",
-    rasterStackCog: "sachsen_raster_stack_web.tif"
+    fqdn: "http://127.0.0.1:8000",
+    // apiBaseUrl: "http://167.233.20.236:8000",
+    // apiBaseUrl: "https://tiles.alleinseinkarte.de",
+    tile_json_path: "WebMercatorQuad/tilejson.json",
+    raster_name: "germany_raster_v1.tif"
 };
