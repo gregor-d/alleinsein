74cb9c3f14641a634ce6ab7bf060b7db686c9216
gregor-d <didenko_g@gmx.de>
2026-06-10 11:22:42 +0200

    fix: update raster endpoint paths in test cases to remove API prefix

--- tests/test_titiler.py
+++ tests/test_titiler.py
@@ -11,29 +11,40 @@ def client():
 
 
 # @pytest.mark.skip()
-def test_raster_endpoint(client):
+def test_raster_default_endpoint(client):
     print("Testing default raster endpoint...")
-    response = client.get("/api/tiles/WebMercatorQuad/0/0/0")
+    response = client.get("/tiles/WebMercatorQuad/0/0/0")
     print(response.headers)
     assert response.status_code == 200
     assert "content-bbox" in response.headers
     assert "content-crs" in response.headers
 
+def test_raster_endpoint(client):
     print("Testing specified raster endpoint...")
-    response = client.get("/api/tiles/WebMercatorQuad/0/0/0?raster=test_raster.tif")
+    response = client.get("/tiles/WebMercatorQuad/0/0/0?raster=test_raster.tif")
     print(response.headers)
     assert response.status_code == 200
     assert "content-bbox" in response.headers
     assert "content-crs" in response.headers
 
+def test_raster_non_existent_endpoint(client):
     print("Testing non-existent raster endpoint...")
     response = client.get(
-        "/api/tiles/WebMercatorQuad/0/0/0?raster=wrong_test_raster.tif"
+        "/tiles/WebMercatorQuad/0/0/0?raster=wrong_test_raster.tif"
     )
     assert response.status_code == 500
 
+def test_raster_secure_endpoint(client):
     print("Testing raster endpoint with directory traversal...")
     response = client.get(
-        "/api/tiles/WebMercatorQuad/0/0/0?raster=../tests/test_raster.tif"
+        "/tiles/WebMercatorQuad/0/0/0?raster=../tests/test_raster.tif"
+    )
+    assert response.status_code == 500
+
+
+def test_tile_json_info(client):
+    print("Testing raster endpoint with directory traversal...")
+    response = client.get(
+        "/tiles/WebMercatorQuad/0/0/0?raster=../tests/test_raster.tif"
     )
     assert response.status_code == 500
